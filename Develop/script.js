38732ad1c09dd08b9af3720b63f02139f8ef5abd
christanfox <bayshorestinks@aol.com>
2022-01-20 19:28:30 -0500

    starting off

--- Develop/script.js
+++ Develop/script.js
@@ -1,17 +1,24 @@
-// Assignment code here
-
-
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+  var generateBtn = document.querySelector("#generate");
+
+  var lowercaseChar = "abcdefghijklmnopqrstuvwxyz";
+  var uppercaseChar = "ABCDEFGHIJKLMNOPQRSTUZWXYZ";
+  var numericalChar = "0123456789";
+  var specialChar   = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  function writePassword() {
+    var password     = generatePassword();
+    var passwordText = document.querySelector("#password");
+
+// Password length user prompt
+  var passwordLength = prompt("Select the amount of characters you would like the password to have. Must be between 8 to 128 characters");
+      passwordLength = parseInt(passwordLength);
+
 
   passwordText.value = password;
 
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+  generateBtn.addEventListener("click", writePassword);
